333fcc941fca85828e7808cc50eacfca8953e4da
XaNNy0 <Janbednarski@outlook.de>
2020-09-18 21:57:19 +0200

    added update button

--- popup.js
+++ popup.js
@@ -1,40 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 	chrome.storage.sync.get({profiles: []}, function(data) {
-		console.log(data)
-		var table = document.createElement('table');
-
-		var tr1 = document.createElement('tr');   
-		var tr2 = document.createElement('tr');  
-
-		var td1 = document.createElement('td');
-		var td2 = document.createElement('td');
-		td2.setAttribute('id', 'td2');
-
-		var td3 = document.createElement('td');
-		var td4 = document.createElement('td');
-		
-		var text1 = document.createTextNode("You: ");
-		var text2 = document.createTextNode(data.profiles[0]);
-
-		const request = getEloFromFaceit(data.profiles[0]).then(elo => { chrome.storage.sync.set({elo}); });
-
-		var text3 = document.createTextNode("Elo: ");
-		getPlayerElo();
-		
-		td1.appendChild(text1);
-		td1.appendChild(text2);
-
-		td2.appendChild(text3);
-
-		tr1.appendChild(td1);
-		tr2.appendChild(td2);
-		
-		table.appendChild(tr1);
-		table.appendChild(tr2);
-		
-		document.body.insertBefore(table, settingsH3)
+		buildTable(data);
 	})
+	
 
 document.getElementById("editFriendsButton").onclick = function() {
 	window.location.assign("editFriends.html");
@@ -48,6 +17,50 @@ function getPlayerElo() {
 		var text = document.createTextNode(data.elo);
 		document.getElementById("td2").append(text);
 	})
+}
+
+function buildTable(data) {
+	var table = document.createElement('table');
+
+	var tr1 = document.createElement('tr');   
+	var tr2 = document.createElement('tr');  
+
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+	td2.setAttribute('id', 'td2');
+
+	var td3 = document.createElement('td');
+	var td4 = document.createElement('td');
+	
+	var text1 = document.createTextNode("You: ");
+	var text2 = document.createTextNode(data.profiles[0]);
+
+	const request = getEloFromFaceit(data.profiles[0]).then(elo => { chrome.storage.sync.set({elo}); });
+
+	var text3 = document.createTextNode("Elo: ");
+	getPlayerElo();
+	
+	td1.appendChild(text1);
+	td1.appendChild(text2);
+
+	td2.appendChild(text3);
+
+	tr1.appendChild(td1);
+	tr2.appendChild(td2);
+	
+	table.appendChild(tr1);
+	table.appendChild(tr2);
+	
+	document.body.insertBefore(table, settingsH3);
+
+	var updateButton = document.createElement('button');
+	updateButton.setAttribute('value', 'Update');
+	updateButton.innerHTML = "Update";
+	updateButton.addEventListener ("click", function() {
+		location.reload();
+	});
+
+	document.body.insertBefore(updateButton,settingsH3);
 }
 
 function getEloFromFaceit(player) {
